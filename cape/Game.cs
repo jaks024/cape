5685bc7f0ff3738ff42ae65777b11f645fe976f3
jaks024 <ye7748560@gmail.com>
2021-11-18 14:55:06 -0500

    initial commit

---
+++ cape/Game.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using OpenTK;
+using OpenTK.Graphics;
+using OpenTK.Graphics.OpenGL;
+using OpenTK.Windowing.Common;
+using OpenTK.Windowing.Desktop;
+using OpenTK.Windowing.GraphicsLibraryFramework;
+
+namespace cape
+{
+	public class Game : GameWindow
+	{
+		public Game(GameWindowSettings gameSettings, NativeWindowSettings  nativeSettings) : base(gameSettings, nativeSettings) { }
+
+		protected override void OnLoad()
+		{
+			GL.ClearColor(0.2f, 0.3f, 0.3f, 1.0f);
+
+			base.OnLoad();
+		}
+
+		protected override void OnRenderFrame(FrameEventArgs args)
+		{
+			GL.Clear(ClearBufferMask.ColorBufferBit);   // clears the screen
+
+
+			Context.SwapBuffers();
+			base.OnRenderFrame(args);
+		}
+
+		protected override void OnResize(ResizeEventArgs e)
+		{
+			GL.Viewport(0, 0, Size.X, Size.Y);
+
+			base.OnResize(e);
+		}
+
+
+		protected override void OnUpdateFrame(FrameEventArgs args)
+		{
+			if (KeyboardState.IsKeyDown(Keys.Escape))
+			{
+				Close();
+			}
+
+			base.OnUpdateFrame(args);
+		}
+	}
+}
---
+++ cape/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using OpenTK;
+using OpenTK.Windowing.Desktop;
+
+namespace cape
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+
+			GameWindowSettings gameSetting = new GameWindowSettings()
+			{
+				IsMultiThreaded = true,
+				RenderFrequency = 60,
+				UpdateFrequency = 60
+			};
+			NativeWindowSettings nativeSetting = new NativeWindowSettings()
+			{
+				Title = "CAPE",
+				Size = new OpenTK.Mathematics.Vector2i(1280, 720)
+			};
+
+			using (Game game = new(gameSetting, nativeSetting))
+			{
+				game.Run();
+			}
+		}
+	}
+}
